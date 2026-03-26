85960c76b9cf41385a7f63bcd62d831883a0ac29
diegofivefive <diegofivefive@gmail.com>
2026-03-26 15:34:27 -0500

    refix ads

--- src/components/layout/adsense-script.tsx
+++ src/components/layout/adsense-script.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Script from "next/script";
 
 const CLIENT_ID = process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID || "ca-pub-7700405385978151";
@@ -12,7 +10,7 @@ export function AdSenseScript() {
       async
       src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${CLIENT_ID}`}
       crossOrigin="anonymous"
-      strategy="afterInteractive"
+      strategy="beforeInteractive"
     />
   );
 }
